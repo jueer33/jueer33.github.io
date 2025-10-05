0be5d35516ca5817847a73b7ac52e4d7bb6dc4a8
jueer33 <jueer33@gmail.com>
2025-10-05 23:16:27 +0800

    'first'

---
+++ docs/.vuepress/navbar.ts
@@ -0,0 +1,30 @@
+/**
+ * @see https://theme-plume.vuejs.press/config/navigation/ 查看文档了解配置详情
+ *
+ * Navbar 配置文件，它在 `.vuepress/plume.config.ts` 中被导入。
+ */
+
+import { defineNavbarConfig } from 'vuepress-theme-plume'
+
+export default defineNavbarConfig([
+  { text: '首页', link: '/' },
+  { text: '博客', link: '/blog/' },
+  // { text: '标签', link: '/blog/tags/' },
+  { text: '归档', link: '/blog/archives/' },
+  { text: '分类', link: '/blog/categories/' },
+  {
+    text: '前端相关',
+    items: [
+      {
+        text: 'vue', items: [
+          { text: '1.vue2响应式原理', link: '/前端相关/vue/1.vue2响应式原理' },
+          { text: '2.vue3响应式原理', link: '/前端相关/vue/2.vue3响应式原理' },
+          { text: '3.vue的diff算法', link: '/前端相关/vue/3.vue的diff算法' },
+          { text: '4.keep-alive原理', link: '/前端相关/vue/4.keep-alive原理' },
+          { text: '5.vue样式隔离原理', link: '/前端相关/vue/5.vue样式隔离原理' },
+          { text: '6.vue press构建', link: '/前端相关/vue/6.vue press构建' }
+        ]
+      }
+    ]
+  },
+])
---
+++ docs/README.md
@@ -0,0 +1,23 @@
+---
+pageLayout: home
+externalLinkIcon: false
+config:
+  -
+    type: hero
+    full: true
+    # background: tint-plate
+    tintPlate: 10
+    hero:
+      name: jueer's blog
+      tagline: 一个技术小白的学习记录
+      text: 前端开发、vue原理、js解析、报错处理
+      actions:
+        -
+          theme: brand
+          text: 博客
+          link: /blog/
+        -
+          theme: alt
+          text: 归档
+          link: /blog/archives/
+---
